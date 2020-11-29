7ada658dddb1f122990c8fab189583d74f70ccea
Meryemessalmi <maryaessalmi@gmail.com>
2020-11-29 16:50:34 +0100

    diagramme de classe

---
+++ Project1/src/newpa/Startpoint.java
@@ -0,0 +1,20 @@
+package newpa;
+import javax.swing.*;
+public class Startpoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Startpoint();
+
+	}
+	public Startpoint() {
+		JFrame f= new JFrame();
+		f.setTitle("Labyrinth game");
+		f.setSize(500,400);
+		f.setLocationRelativeTo(null);
+		f.add(new Board());
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+}
+}
---
+++ Project1/src/newpa/Tresor.java
@@ -0,0 +1,21 @@
+package newpa;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Tresor {
+	public int x;
+	public int y;
+	private Image tres;
+	Tresor(int x1, int y1){
+		ImageIcon img= new ImageIcon(getClass().getResource("tresor.jpg"));
+		tres=img.getImage();
+		x=x1;
+		y=y1;
+		
+}
+	public Image gettresor() {
+		return tres;
+	}
+	
+}
